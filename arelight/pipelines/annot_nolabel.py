6e472affb4d3b3ffeebc60a4a4caa8e533dd2dca
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-08-20 11:51:34 +0300

    Updated pipeline implementation (sync with AREkit changes)

--- arelight/pipelines/annot_nolabel.py
+++ arelight/pipelines/annot_nolabel.py
@@ -1,16 +1,20 @@
 from arekit.common.labels.base import NoLabel
 from arekit.common.labels.provider.constant import ConstantLabelProvider
 from arekit.common.opinions.annot.algo.pair_based import PairBasedOpinionAnnotationAlgorithm
-from arekit.common.opinions.annot.algo_based import AlgorithmBasedOpinionAnnotator
 from arekit.common.opinions.collection import OpinionCollection
 from arekit.common.synonyms.grouping import SynonymsCollectionValuesGroupingProviders
-from arekit.contrib.utils.pipelines.annot.base import attitude_extraction_default_pipeline
+from arekit.contrib.utils.pipelines.text_opinion.annot.algo_based import AlgorithmBasedTextOpinionAnnotator
+from arekit.contrib.utils.pipelines.text_opinion.extraction import text_opinion_extraction_pipeline
+from arekit.contrib.utils.pipelines.text_opinion.filters.distance_based import DistanceLimitedTextOpinionFilter
 
 
 def create_neutral_annotation_pipeline(synonyms, dist_in_terms_bound, terms_per_context,
                                        doc_ops, text_parser, dist_in_sentences=0):
 
-    annotator = AlgorithmBasedOpinionAnnotator(
+    nolabel_annotator = AlgorithmBasedTextOpinionAnnotator(
+        value_to_group_id_func=lambda value:
+            SynonymsCollectionValuesGroupingProviders.provide_existed_or_register_missed_value(
+                synonyms=synonyms, value=value),
         annot_algo=PairBasedOpinionAnnotationAlgorithm(
             dist_in_sents=dist_in_sentences,
             dist_in_terms_bound=dist_in_terms_bound,
@@ -22,14 +26,14 @@ def create_neutral_annotation_pipeline(synonyms, dist_in_terms_bound, terms_per_
             error_on_synonym_end_missed=False),
         get_doc_existed_opinions_func=lambda _: None)
 
-    annotation_pipeline = attitude_extraction_default_pipeline(
-        annotator=annotator,
-        get_doc_func=lambda doc_id: doc_ops.get_doc(doc_id),
+    annotation_pipeline = text_opinion_extraction_pipeline(
         text_parser=text_parser,
-        value_to_group_id_func=lambda value:
-        SynonymsCollectionValuesGroupingProviders.provide_existed_or_register_missed_value(
-            synonyms=synonyms, value=value),
-        terms_per_context=terms_per_context,
-        entity_index_func=None)
+        get_doc_func=lambda doc_id: doc_ops.get_doc(doc_id),
+        annotators=[
+            nolabel_annotator
+        ],
+        text_opinion_filters=[
+            DistanceLimitedTextOpinionFilter(terms_per_context)
+        ])
 
     return annotation_pipeline
