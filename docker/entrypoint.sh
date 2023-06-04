7f118fab2661f4174a29cde652ab5161f7f36677
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-06-04 22:21:46 +0100

    Dropping docker support #72

--- docker/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo
-export PYTHONPATH="/arelight"
-
-$@
--- docker/start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Cleaning up previous containers"
-docker rm -f arelight || true
-
-docker run --gpus all -e nicolay-r/arelight
