e130da4e1cf8bb083ef555de3a3ae3a297c856f7
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-10-18 11:22:29 +0100

    Refactoring `graph_to_radial` function

--- arelight/backend/d3js/utils_graph.py
+++ arelight/backend/d3js/utils_graph.py
@@ -7,18 +7,26 @@ def graph_to_radial(graph):
     """ (C) Maxim Kolomeets
     """
     nodes_ = {}
+
     for n in graph["nodes"]:
         nodes_[n["id"]] = {"w": n["c"]}
+
     for l in graph["links"]:
+
         if l["target"] not in nodes_:
             continue
-        elif "imports" not in nodes_[l["target"]]:
-            nodes_[l["target"]]["imports"] = []
-        nodes_[l["target"]]["imports"].append({
+
+        target = nodes_[l["target"]]
+
+        if "imports" not in target:
+           target["imports"] = []
+
+        target["imports"].append({
             "name": l["source"],
             "w": l["c"],
             "sent": l["sent"]
         })
+
     nodes = []
     for n_ in nodes_:
         n = nodes_[n_]
